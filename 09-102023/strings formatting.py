79ce75bfb7d011a01ed6f94a05497b00635c5581
Naeem-akber <70134245@student.uol.edu.pk>
2023-10-09 15:06:37 +0500

    newcode6

--- 09-102023/strings formatting.py
+++ 09-102023/strings formatting.py
@@ -1,3 +1,3 @@
-name ="Manny"
-number = len(name)*3
-print("Hello{},your luck number{}".format(name,number))
+name ="love"
+number = len(name)*5
+print("Hello{number},your luck number{name}".format(name="name",number="number"))
