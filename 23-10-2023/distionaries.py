518ad02942eeb03bd99407cc7786118be6601df2
Naeem-akber <70134245@student.uol.edu.pk>
2023-10-23 15:25:21 +0500

    handling

--- 23-10-2023/distionaries.py
+++ 23-10-2023/distionaries.py
@@ -2,4 +2,4 @@ file_counts ={"jpg":1,"text":14,"csv":2,"py":23}
 for extension in file_counts:
   print(extension)
 for ext,amount in file_counts.items():
-    print("thereare{}files with the.{}extension".format(amount,ext))
+    print("there are {}files with the.{}extension".format(amount,ext))
